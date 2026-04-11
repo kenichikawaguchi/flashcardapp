7385c56073a37147b5a2439ffe528538a66f5b1e
kenichikawaguchi <xianzaikaishiba@gmail.com>
2026-04-11 13:40:33 +0900

    Modify published_date

--- app/routes.py
+++ app/routes.py
@@ -507,6 +507,8 @@ def article_list():
 
 @main.route('/articles/<slug>')
 def article(slug):
+    mtime = os.path.getmtime(path)
+    published_date = datetime.datetime.fromtimestamp(mtime).strftime('%Y年%-m月%-d日')
     path = os.path.join(main.root_path, 'content', 'articles', f'{slug}.md')
     with open(path, encoding='utf-8') as f:
         raw = f.read()
